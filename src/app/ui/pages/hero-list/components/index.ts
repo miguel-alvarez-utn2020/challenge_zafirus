b623985005f1c320604d2a8f84006cec3aaedc45
Miguel Alvarez <miguel-alvarez-utn2020>
2024-04-25 19:06:54 -0300

    fix img

--- src/app/ui/pages/hero-list/components/index.ts
+++ src/app/ui/pages/hero-list/components/index.ts
@@ -1,1 +1,4 @@
-export * from './components.module'
+export * from './components.module';
+export * from './hero-card/hero-card.component';
+export * from './hero-card-skeleton/hero-card-skeleton.component';
+export * from './item-hero-list/item-hero-list.component';
